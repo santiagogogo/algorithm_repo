d11e04f4941f63c07e5f350d569393076ab99c3f
葛德来 <gedelai@xin.com>
2018-11-30 11:40:05 +0800

    创建算法项目

---
+++ src/main/java/com/lailai/algorithm_repo/AlgorithmRepoApplication.java
@@ -0,0 +1,12 @@
+package com.lailai.algorithm_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmRepoApplication.class, args);
+	}
+}
