79def8ff07531352fc7b491c6e2aa06703322ed0
santiagogogo <gedelai@sina.com>
2019-06-30 19:33:30 +0800

    add-memo-pattern

---
+++ src/main/java/com/lailai/algorithm_repo/MemoPattern/Memo.java
@@ -0,0 +1,17 @@
+package com.lailai.algorithm_repo.MemoPattern;
+
+public class Memo {
+    private String sts;
+
+    public Memo(String sts) {
+        this.sts = sts;
+    }
+
+    public String getSts() {
+        return sts;
+    }
+
+    public void setSts(String sts) {
+        this.sts = sts;
+    }
+}
