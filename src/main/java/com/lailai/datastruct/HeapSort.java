a65fa91b45912cdbc5f5b72938141d4c055c02d0
ge.delai <ge.delai@immomo.com>
2021-09-24 22:43:44 +0800

    heapsort

--- src/main/java/com/lailai/datastruct/HeapSort.java
+++ src/main/java/com/lailai/datastruct/HeapSort.java
@@ -1,5 +1,7 @@
 package com.lailai.datastruct;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.Arrays;
 
 /**
@@ -15,7 +17,7 @@ public class HeapSort {
      * 3.对新的长度-1的堆，重新递归排序
      */
 
-    private void swap(int[] arr,int j,int i){
+    public static void swap(int[] arr,int j,int i){
         int tmp=arr[i];
         arr[i]=arr[j];
         arr[j]=tmp;
@@ -24,17 +26,17 @@ public class HeapSort {
     /**
      * 递归选出该堆最大节点（根节点）
      */
-    private void getMaxRoot(int[] arr,int curr,int size){
+    public static void getMaxRoot(int[] arr,int curr,int size){
         if (curr>size-1){
             return;
         }
         int left=curr*2+1;
         int right=curr*2+2;
         int max=curr;
-        if (left<size&&arr[left]<arr[max]){
+        if (left<size&&arr[left]>arr[max]){
             max=left;
         }
-        if (right<size&&arr[right]<arr[max]){
+        if (right<size&&arr[right]>arr[max]){
             max=right;
         }
         if (max!=curr){
@@ -43,10 +45,29 @@ public class HeapSort {
         }
     }
 
-    public void getMaxHeap(int[] arr){
-        for (int i = arr.length-1;i>=0;i--){
-            getMaxRoot(arr,i,arr.length);
+    public static void getMaxHeap(int[] arr,int length){
+        for (int i = length-1;i>=0;i--){
+            getMaxRoot(arr,i,length);
         }
+        swap(arr,0,length-1);
     }
+
+    public static void pullFirst(int[] arr){
+        int length=arr.length;
+        for (int i=0;i<length-1;i++){
+            getMaxHeap(arr,length-i);
+        }
+        System.out.println(JSON.toJSONString(arr));
+    }
+
+    public static void main(String[] args) {
+        int[] arr={44,3,45,6,77,543,443,556,2};
+        pullFirst(arr);
+        int[] arr2={2,5,3,10,4};
+        pullFirst(arr2);
+
+    }
+
+
 
 }
